03326b5d4b24befdb3019e40129ba17edb82de1b
sblyufer <sblyufer24@gmail.com>
2021-11-26 20:49:49 +0200

    Let's go yo yo

--- AVLTree.h
+++ AVLTree.h
@@ -13,32 +13,16 @@ class AVLTree{
     int max(int x, int y){
         return x > y ? x : y;
     }
-    void RR(BTreeNOde* node);
+    void RR(BTreeNOde* node); // sofi
     void LL(BTreeNOde* node);
 
 public:
-    int getBalance(BTreeNode* node){
-        int l,r;
-        if (node -> goLeft() == nulptr)
-        {
-            l = 0;
-        }
-        else{
-            l = node -> getLHeight();
-        }
-        if (node -> goRight() == nullptr)
-        {
-            r = 0;
-        }
-        else{
-            r = node -> getRHeight();
-        }
-
-        return (l-r);
+    int getBalance(){
+        return getHeight(source->goLeft()) - getHeight(source->goRight());
     }
-    void insert(const T& value);
+    void insert(const T& value); // sofi
     void delete(const T& value);
     BTreeNode* find(const T& value);
-    void inOrder();
+    void inOrder(); // sofi
 };
 #endif //PLAYERSMANAGER_AVLTREE_H
--- BTreeNode.h
+++ BTreeNode.h
@@ -6,6 +6,8 @@ class BTreeNode{
     T value;
     BTreeNode* r_child, l_child;
     int height;
+
+
 public:
     explicit BTreeNode(const T& _value) : value(_value), r_child(nullptr), l_child(nullptr), height(1);
     ~BTreeNode() = default;
@@ -19,27 +21,20 @@ public:
     void setHeight(){
         height = getLHeight() - getRHeight() + 1;
     }
-    int getHeight(){
-        return height;
-    }
-    int getLHeight(){
-        if (l_child == nullptr){
-            return 0;
-        }
-        return l_child->getHeight();
-    }
-    int getRHeight(){
-        if (r_child == nullptr){
-            return 0;
-        }
-        return r_child->getHeight();
-    }
     void setLChild(BTreeNode* _l_child){
         l_child = _l_child;
     }
     void setRChild(BTreeNode* _r_child){
         r_child = _r_child;
     }
+    int getHeight() {
+        return node->height;
+    }
 };
-
+    int getHeight(BTreeNode* node){
+        if (node == nullptr){
+            return 0;
+        }
+        return node->getHeight();
+    }
 #endif
